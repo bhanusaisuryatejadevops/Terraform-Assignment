70795f76e93697fc9bef6947ff142b18b67ab224
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2023-12-29 19:21:28 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -12,13 +12,3 @@ variable "ec2_key_name" {
   description = "Key pair name for EC2 SSH access"
   default     = "my-keypair"
 }
-
-variable "ec2_security_group_id" {
-  description = "ID of the security group associated with EC2"
-  # Add your security group ID or provide it during runtime
-}
-
-variable "ec2_subnet_id" {
-  description = "ID of the subnet where EC2 will be launched"
-  # Add your subnet ID or provide it during runtime
-}
