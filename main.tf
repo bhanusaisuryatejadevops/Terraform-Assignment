c0fe59337ebe021513073661b83832c8d470ddd7
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2023-12-29 19:26:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,7 @@ module "vpc" {
 }
 module "ec2" {
   source = "./ec2" 
-  depends_on = [
-    module.vpc
-  ]
-  subnetpublic = module.vpc.pub_subnets
-  sgs = module.vpc.security_group_ic
 }
 module "rds" {
   source = "./rds" 
-  sgs=module.vpc.security_group_ic
-  dbsubgrp = module.vpc.private_subnet_cidrs
-  depends_on = [
-    module.vpc
-  ]
 }
