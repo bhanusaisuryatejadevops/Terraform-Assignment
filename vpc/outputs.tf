316882e100f196700334323e080c1e0d82bf3236
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2024-01-03 18:44:17 +0530

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,8 +1,29 @@
-output "pub_subnets" {
-    value = aws_subnet.subnet_pub.*.id
-  
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.myvpc.id
 }
-output "security_group_ic" {
-    value = aws_security_group.sg.id
-  
+
+output "public_subnet_ids" {
+  description = "List of IDs of the created public subnets"
+  value       = aws_subnet.subnet_pub[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of IDs of the created private subnets"
+  value       = aws_subnet.subnet_pri[*].id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the created Internet Gateway"
+  value       = aws_internet_gateway.myigw.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the created NAT Gateway"
+  value       = aws_nat_gateway.icnat.id
+}
+
+output "default_security_group_id" {
+  description = "The ID of the default security group"
+  value       = aws_security_group.default.id
 }
