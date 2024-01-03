bfef07332b8d30d48abed05bee7b3f85575187a9
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2024-01-03 18:58:55 +0530

    Update variables.tf

--- rds/variables.tf
+++ rds/variables.tf
@@ -1,19 +1,27 @@
-variable "rds_instance_identifier" {
-  description = "Identifier for the RDS instance"
-  default     = "my-rds-instance" 
+variable dbsubgrp {
+    type = list(string)
+
 }
 
-variable "rds_engine" {
-  description = "RDS database engine (e.g., postgres)"
-  default     = "postgres" 
+variable "sgs" {
+    
+  
 }
 
-variable "rds_engine_version" {
-  description = "RDS database engine version"
-  default     = "13.4"  
+variable engine {
+    default = "postgres"
 }
 
-variable "rds_instance_class" {
-  description = "RDS instance class"
-  default     = "db.t2.micro" 
+variable "engine_version" {
+    default = "10.14"
+  
+}
+
+variable "username" {
+    default = "rdsadmin"
+  
+}
+
+variable "instanceclass" {
+  default = "db.t2.micro"
 }
