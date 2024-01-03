20c95ec2b0b243d6821802c11831692fce7c43e8
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2024-01-03 18:41:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
 module "vpc" {
   source = "./modules/vpc"
 }
+
+# VPC Configuration (Place your existing VPC configuration here)
+
 module "ec2" {
-  source = "./ec2" 
+  source    = "./modules/ec2"
+  depends_on = [
+    module.vpc
+  ]
+  subnetpublic = module.vpc.pub_subnets
+  sgs = [module.vpc.aws_security_group.sg.id]
 }
+
 module "rds" {
-  source = "./rds" 
+  source    = "./modules/rds"
+  
+depends_on = [
+    module.vpc
+  ]
+  sgs = [module.vpc.aws_security_group.sg.id]
+  dbsubgrp = module.vpc.pri_subnets
+
 }
