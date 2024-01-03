20d39966963092eb25e0b6da6a7bc10a0d7c6fd8
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2024-01-03 19:02:06 +0530

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,15 +1,3 @@
-output "ec2_instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = aws_instance.ec2.id
+output "ec2pubip" {
+    value = aws_instance.web.public_ip
 }
-
-output "ec2_instance_public_ip" {
-  description = "Public IP address of the created EC2 instance"
-  value       = aws_instance.ec2.public_ip
-}
-
-output "ec2_instance_private_ip" {
-  description = "Private IP address of the created EC2 instance"
-  value       = aws_instance.ec2.private_ip
-}
-
