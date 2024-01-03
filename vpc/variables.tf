18abd96bfc858b7ac202991147e5aca1e9df8390
bhanusaisuryatejadevops <136581345+bhanusaisuryatejadevops@users.noreply.github.com>
2024-01-03 18:44:36 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,16 +1,18 @@
 variable "pubsubnet" {
-    description = "CIDR blocks for public subnets"
-    default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]  
 }
 
 variable "prisubnet" {
-    description = "CIDR blocks for private subnets"
-    default = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
-  
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]  
 }
 
 variable "azones" {
-  description = "Availability zones"
-  default = ["us-west-2a","us-west-2b","ap-south-1"]
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "ap-south-1"]
 }
+
